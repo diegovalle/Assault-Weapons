c9fe9f3019555305e201781db7b908dfee78e21c
Diego Valle Jones <diegovalle@gmail.com>
2010-07-14 11:55:17 -0500

    adds suicides, homicides by region and readme.md

--- assault-weapons.r
+++ assault-weapons.r
@@ -62,18 +62,18 @@ plotAsWeBreaks <- function(df, breaks.df, ban, sub = NULL,
   df$group <- df$date.x < df$date.y
   df$State <- reorder(df$State, abs(df$date.y - ban))
   p <- ggplot(df, aes(as.Date(date.x), prop.x, group = State)) +
-    geom_line(size = .2) +
-    scale_x_date(major="2 years") +
-    geom_smooth(se = FALSE, aes(group = group), method = lm) +
-    opts(axis.text.x=theme_text(angle=60, hjust=1.2 )) +
     geom_vline(aes(xintercept = as.Date(date.y)),
                color = "red", alpha = .7) +
     geom_vline(aes(xintercept = as.Date(ban)), color = "gray",
                linetype = 2) +
+    geom_line(size = .2) +
+    scale_x_date(major="2 years") +
+    geom_smooth(se = FALSE, aes(group = group), method = lm) +
+    opts(axis.text.x=theme_text(angle=60, hjust=1.2 )) +
     opts(title = plottitle) +
     xlab("date") + ylab("proportion") +
-    scale_y_continuous(formatter = "percent",
-                       breaks = c(.25, .5, .75)) +
+    scale_y_continuous(formatter = "percent"
+                       ) +
     facet_wrap(~ State)#, scale = "free_y")
   if (!se) {
       p
@@ -97,7 +97,7 @@ ban <- as.Date("2004/09/13", "%Y/%m/%d")
 #Read the data
 ########################################################
 #hom <- read.csv(file("http://spreadsheets.google.com/pub?key=0AjjLwVYbDGx7dHdaMHhCQ21OZ1VzQk42WnltUXFkUWc&single=true&gid=0&output=csv",encoding = "UTF8"))
-hom <- read.csv(bzfile("guns-month.csv.bz2"))
+hom <- read.csv(bzfile("data/guns-month.csv.bz2"))
 hom <- subset(hom, Month != "No Especificado")
 hom <- hom[-grep("Total", hom$State),]
 hom$Year <- rep(1998:2008, each = 12)
@@ -111,7 +111,7 @@ hom$State <- cleanNames(hom, "State")
 hom <- subset(hom, m != 12 | Year != 2008)
 
 #Monthly Population Interpolation
-pop <- read.csv("conapo-pop.csv")
+pop <- read.csv("data/conapo-pop.csv")
 pop2 <- data.frame(year = rep(1998:2008, each = 12),
                    month = rep(1:12))
 pop2$Monthly.Pop[pop2$month == 6] <- unlist(pop[1,1:ncol(pop)])
@@ -126,6 +126,72 @@ unitRoot <- function(df){
 }
 #Looks ok
 dlply(hom, .(State), unitRoot)
+
+
+
+########################################################
+#Suicides and Suicides with a Firearm
+########################################################
+sui <- read.csv("data/suicides.csv")
+sui <- subset(sui, Month != "No Especificado")
+sui$Year <- rep(1998:2008, each = 12)
+sui$m <- rep(1:12)
+sui$prop <- sui$Firearm.Suicides / sui$Suicides
+
+sui$date <- as.Date(paste(sui$Year,
+                             sui$m,"15", sep = "/"), "%Y/%m/%d")
+
+sui$trend2 <- data.frame(stl(ts(sui$prop, start = 1998, freq = 12), "per")$time.series)$trend
+
+p <- ggplot(sui, aes(date, prop)) +
+    geom_line(size = .2) +
+    geom_line(data = sui, aes(date, trend2), color = "blue") +
+    scale_x_date() +
+    geom_vline(aes(xintercept = as.Date(ban)), color = "gray70",
+               linetype = 2) +
+    opts(title = "Proportion of Suicides committed with a Firearm (the gray line corresponds to the assault weapon ban expiration)") +
+    scale_y_continuous(limits = c(0, .27)) +
+    xlab("date") + ylab("annualized monthly suicide rate")
+savePlotAA(p, "output/mexico-sui-firearm-prop.png")
+
+sui.both <- sui
+sui.both[3:4] <- data.frame(sapply(sui.both[3:4],
+                function(x) x / pop2$Monthly[1:nrow(sui.both)] *
+                                100000 * 12))
+sstl <- apply(sui.both[3:4], 2,
+      function(x) {
+        stl(ts(x, start = 1998, freq = 12), "per")
+      })
+
+sstl <- lapply(sstl, function(x)
+                      cbind(sui.both, data.frame(x$time.series)))
+
+sui.both <- melt(sui.both, id = c("Year", "m"),
+                 measure.var = c("Firearm.Suicides",
+                 "Suicides"))
+sui.both$date <- as.Date(paste(sui.both$Year,
+                             sui.both$m,"15", sep = "/"), "%Y/%m/%d")
+#Necessary for ggplot
+sstl$Firearm.Suicides$date <- hom.both$date[1:132]
+sstl$Suicides$date <- hom.both$date[1:132]
+sstl$Firearm.Suicides$variable <- "foo"
+sstl$Suicides$variable <- "foo"
+
+p <- ggplot(sui.both, aes(date, value, group = variable)) +
+    geom_line(size = .2) +
+    geom_line(data = sstl$Firearm.Suicides, aes(date, trend),
+              color = "blue") +
+    geom_line(data = sstl$Suicides, aes(date, trend), color = "blue") +
+    scale_x_date() +
+    geom_vline(aes(xintercept = as.Date(ban)), color = "gray70",
+               linetype = 2) +
+    opts(title = "Suicides and Suicides with a Firearm") +
+    annotate("text", x= as.Date("1999-01-15"), y = 4.2,
+             label ="Suicides") +
+    annotate("text", x= as.Date("2000-01-15"), y = 1.1,
+             label ="Suicides with a Firearm") +
+    xlab("date") + ylab("annualized monthly suicide rate")
+savePlotAA(p, "output/mexico-sui-firearm.png")
 
 ########################################################
 #Homicide and Firearm Homicide rates
@@ -164,6 +230,11 @@ p <- ggplot(hom.both, aes(date, value, group = variable)) +
     geom_vline(aes(xintercept = as.Date(ban)), color = "gray70",
                linetype = 2) +
     opts(title = bothtitle) +
+    scale_y_continuous(limits = c(0, 17)) +
+    annotate("text", x= as.Date("2001-06-15"), y = 12,
+             label ="Homicides") +
+    annotate("text", x= as.Date("2002-01-15"), y = 7,
+             label ="Homicides with a Firearm") +
     xlab("date") + ylab("annualized monthly homicide rate")
 savePlotAA(p, "output/mexico-hom-firearm.png")
 
@@ -191,9 +262,58 @@ p <- ggplot(hom.mx, aes(date, V1)) +
                linetype = 2) +
     opts(title = plottitle) +
     xlab("date") + ylab("proportion") +
-    scale_y_continuous(formatter = "percent")
-savePlotAA(p, "output/mexico.png")
+    scale_y_continuous(formatter = "percent", limits = c(0, .8))
+savePlotAA(p, "output/mexico-prop.png")
 
+
+########################################################
+#For the different regions (SW NW, etc)
+########################################################
+hom.region <- read.csv("data/regions.csv")
+hom.region$Region <- rep(c("CE Center-East",
+"CW Center West", "NE North East", "No disponible", "NW Northwest",
+"S South"), each = 13*11)
+hom.region <- subset(hom.region, Month != "No Especificado" &
+                                 Region != "No disponible")
+hom.region$date <- seq(as.Date("1998-01-15"), by='1 month',
+                       length=12*11)
+hom.region$prop <- hom.region$Firearm.Homicide / hom.region$Homicide
+hom.region$trend <- data.frame(stl(ts(hom.region$prop, start = 1998, freq = 12), "per")$time.series)$trend
+
+hom.region <- ddply(hom.region, .(Region), transform,
+                    order = mean(prop))
+hom.region$Region <- reorder(hom.region$Region, -hom.region$order)
+p <- ggplot(hom.region, aes(date, prop)) +
+       geom_vline(aes(xintercept = as.Date(ban)), color = "gray70",
+               linetype = 2) +
+       geom_line(size = 0.2) +
+       geom_line(aes(date, trend), color = "blue") +
+       scale_x_date(major = "2 years") +
+       facet_wrap(~ Region) +
+       xlab("date") + ylab("Proportion of Firearm Homicides") +
+       opts(title="Proportion Firearm Homicides in the Different Geographical Regions of Mexico")+
+       scale_y_continuous(formatter = "percent", limits = c(0, .8))
+savePlotAA(p, "output/regions.png")
+
+########################################################
+#For Municipalities near the US Border
+########################################################
+hom.border <- read.csv("data/frontier.csv")
+hom.border$date <- seq(as.Date("1998-01-15"), by='1 month',
+                       length=12*11)
+hom.border$prop <- hom.border$Firearm.Homicide / hom.border$Homicide
+hom.border$trend <- data.frame(stl(ts(hom.border$prop, start = 1998, freq = 12), "per")$time.series)$trend
+
+p <- ggplot(hom.border, aes(date, prop)) +
+       geom_vline(aes(xintercept = as.Date(ban)), color = "gray70",
+               linetype = 2) +
+       geom_line(size = 0.2) +
+       geom_line(aes(date, trend), color = "blue") +
+       scale_x_date(major = "2 years") +
+       xlab("date") + ylab("Number of Firearm Homicides") +
+       opts(title="Proportion Firearm Homicides in Mexican Municipalities Near the US Border") +
+       scale_y_continuous(formatter = "percent", limits = c(0, .9))
+savePlotAA(p, "output/us-border.png")
 
 ########################################################
 #Small Multiples of all the states with breakpoints
@@ -210,19 +330,21 @@ breaks.df$prop <- 0
 
 dts <- c("Chihuahua", "Sinaloa", "Durango", "Sonora",
          "Guerrero", "Baja California","Michoacán", "Tamaulipas")
-st <- c("Aguascalientes", "México", "Chiapas", "Morelos", "Nuevo León",
+st <- c("México", "Chiapas", "Puebla", "Nuevo León",
         "Quintana Roo")
 
 
-filenames <- c("all", "sonora", "dts", "interesting")
+filenames <- c("all", "chihuahua", "dts", "interesting")
 filenames <- sapply(filenames,
                     function(x) paste("output/", x,
                                       ".png", sep = ""))
+widths <- c(960, 640, 800, 800)
+heights <- c(600, 480, 600, 600)
 
-
-mapply(function(x, y, z)
-            savePlotAA(plotAsWeBreaks(hom, breaks.df, ban, x, z), y),
-       list(NULL, "Sonora", dts, st), filenames,
-       list(TRUE, FALSE, FALSE, FALSE)
+mapply(function(x, y, z, height, width)
+            savePlotAA(plotAsWeBreaks(hom, breaks.df, ban, x, z), y,
+                       height, width),
+       list(NULL, "Chihuahua", dts, st), filenames,
+       list(TRUE, FALSE, FALSE, FALSE), widths, heights
       )
 
---
+++ readme.md
@@ -0,0 +1,41 @@
+The effect of the expiration of the assault weapon ban on Mexico
+-----------------------------------------------------------------
+
+
+Sources
+=======
+
+__La Barbie__
+>In September 2004, he bought a full-page ad in the Monterrey newspaper El Norte and in an open letter declared that he was merely a businessman. He asked President Vicente Fox for justice and said he had left Nuevo Laredo because police there kept pressing him for bribes.
+[The Dallas Morning News](http://www.dallasnews.com/sharedcontent/dws/news/dmn/stories/032106dnintlabarbie.2c3e434.html)
+
+__
+>En él iban Rodolfo Carrillo Fuentes y su esposa; habían sido ejecutados por un comando en el estacionamiento del centro comercial Cinépolis en Culiacán, el 11 de septiembre de 2004.
+
+>El encargado de la operación que acabó con la vida del Niño de Oro —hermano del fallecido Señor de los Cielos, Amado Carrillo—, su mujer y seis personas más —entre ellas un lavacoches— era un oficial de infantería desertor del Ejército, quien a partir de esa fecha se volvió célebre en el mundo de los sicarios por discreto y eficaz y por haber realizado el ataque con el que su jefe, Joaquín El Chapo Guzmán, reventó su relación con los Carrillo.
+[La infantería ligera del ‘Chapo’](http://www.eluniversal.com.mx/nacion/159407.html)
+
+__Homicides with Firearm__
+
+Dirección General de Información en Salud (DGIS). Base de datos de defunciones 1979-2007. [en línea]: Sistema Nacional de Información en Salud (SINAIS). [México]: Secretaría de Salud. <http://www.sinais.salud.gob.mx> [Consulta: 01 abril 2009].
+
+Firearms-related homicides: 
+
+* X93 Assault by handgun discharge
+* X94 Assault by rifle, shotgun and larger firearm discharge
+* X95 Assault by other and unspecified firearm discharge
+
+__Suicides with Firearm__
+
+Dirección General de Información en Salud (DGIS). Base de datos de defunciones 1979-2007. [en línea]: Sistema Nacional de Información en Salud (SINAIS). [México]: Secretaría de Salud. <http://www.sinais.salud.gob.mx> [Consulta: 01 abril 2009].
+
+Firearms-related suicides: 
+
+* X72 Intentional self-harm by handgun discharge
+* X73 Intentional self-harm by rifle, shotgun and larger firearm discharge
+* X74 Intentional self-harm by other and unspecified firearm discharge
+
+__Population Data:__
+
+[Indicadores demográficos básicos 1990-2030](http://www.conapo.gob.mx/index.php?option=com_content&view=article&id=125&Itemid=203)
+
